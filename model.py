8dfbe0f85cffc7b766c399ae7d09920b3922f76a
Hanna <hanna.ei90@gmail.com>
2022-05-21 14:49:39 +0200

    testat 20tusen saker

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
+import json
+
 
 def waiting_num(ticket_num, current_num):
     return ticket_num - current_num
 
 number = [0]
-
+dictionary ={}
 
 def store_ticket_num(ticket_num):
     number.append(ticket_num)
@@ -14,7 +16,28 @@ def store_current_num(current_num):
     return current
 
 def get_number():
-    y_number = number.pop()
+    copy_number = number[:]
+    y_number = copy_number.pop()
     y_number_int = int(y_number)
     return y_number_int
 
+def load():
+    global dictionary
+    try:
+        with open('que.dat') as f:
+            dictionary = json.load(f)
+    except:
+        dictionary = {}
+
+def save_dictionary():
+    with open('que.dat', 'w') as f:
+        json.dump(dictionary, f)
+
+def store_in_dictionary(key, value,):
+    dictionary[key] = [dictionary[key], value]
+
+
+def get_dictionary():
+    current = dictionary.get(value, default=None)
+    return current
+            
