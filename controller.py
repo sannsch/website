a93f6412318c8a6416831432ad448de454e8ee98
Hanna <hanna.ei90@gmail.com>
2022-05-21 15:14:48 +0200

    rensat

--- controller.py
+++ controller.py
@@ -1,39 +1,13 @@
 import requests
-from flask_socketio import SocketIO, emit
-from flask import Flask, request, render_template, redirect, flash, url_for, copy_current_request_context, jsonify
+
+from flask import Flask, request, render_template, redirect, flash, jsonify
 from model import waiting_num, store_ticket_num, get_number#, store_in_dictionary, save_dictionary, get_dictionary, load
 import time
-from threading import Thread, Event
-
 
 
 app = Flask(__name__)
 app.secret_key = 'the random string'
 
-# socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
-# 
-# thread = Thread()
-# thread_stop_event = Event()
-# 
-# def number_generator():
-#     while not thread_stop_event.isSet():
-#         number_current = 10
-#         print(number_current)
-#         socketio.emit('newnumber', {'number': number_current}, namespace='/test')
-#         return number_current
-#         number_current +=1
-#         socketio.sleep(10)
-# 
-# @socketio.on('connect', namespace='/test')
-# def test_connect():
-#     global thread
-# 
-#     if not thread.is_alive():
-#         thread = socketio.start_backgound_task(number_generator())
-# 
-# @socketio.on('disconnect', namespace='/test')
-# def test_disconnect():
-#     print('Client disconnected')
 
 @app.route('/')
 def home():
@@ -53,17 +27,12 @@ def add_place():
         flash('Butiken du har valt finns inte i listan för tillgängliga butiker')
         return render_template('place.html')
 
+
 @app.route('/add_ticket', methods=['POST'])
 def add_ticket():
     ticket_num = request.form['ticket_num']
     store_ticket_num(ticket_num)
-
     return redirect('/show_ticket')
-
-# @app.route('/stuff', methods = ['GET'])
-# def stuff():
-#     return jsonify(result=time.time())
-
 
 
 @app.route('/show_ticket')
@@ -71,6 +40,7 @@ def add_ticket():
 def show_ticket():
     your_number =get_number()
     result = jsonify(time.time())
+    print (result)
 
     
     # waiting = waiting_num(your_number, current_num)
@@ -78,18 +48,6 @@ def show_ticket():
 
 
 if __name__ == '__main__':
-    # socketio.run(app)
     app.run(debug=True, port=5000)
 
-    #
-    # key = request.args['key']
-    # value = request.args['value']
-    # store_in_dictionary(key,value)
-    # save_dictionary()
-    # current = get_dictionary()
 
-    # key = request.args.get('key')
-    # value = request.args.get('value')
-    # store_in_dictionary(key, value)
-    # save_dictionary()
-    # current = get_dictionary()
